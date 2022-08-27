f035f7092ac46dad5500eadbc23a57b2bc534792
Roshan Mykoo <roshanjmykoo@gmail.com>
2022-08-26 22:39:53 -0400

    added view controllers and buttons
    added segues to take user to different screens to showcase different uses of the iphone's camera

---
+++ CameraDemo/CameraDemo/QRViewController.swift
@@ -0,0 +1,29 @@
+//
+//  QRViewController.swift
+//  CameraDemo
+//
+//  Created by Roshan Mykoo on 8/26/22.
+//
+
+import UIKit
+
+class QRViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- CameraDemo/CameraDemo/ViewController.swift
+++ CameraDemo/CameraDemo/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var pictureSegueButton: UIButton!
+    @IBOutlet weak var videoSegueButton: UIButton!
+    @IBOutlet weak var qrSequeButton: UIButton!
+    
+    // mainToPicture
+    // mainToVideo
+    // mainToQR
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        pictureSegueButton.layer.cornerRadius = 5
+        videoSegueButton.layer.cornerRadius = 5
+        qrSequeButton.layer.cornerRadius = 5
     }
-
-
+    
+    @IBAction func pictureButtonCLick(_ sender: Any) {
+        performSegue(withIdentifier: "mainToPicture", sender: self)
+    }
+    
+    
+    @IBAction func videoButtonClick(_ sender: Any) {
+        performSegue(withIdentifier: "mainToVideo", sender: self)
+    }
+    
+    @IBAction func qrButtonClick(_ sender: Any) {
+        performSegue(withIdentifier: "mainToQR", sender: self)
+    }
+    
 }
 
