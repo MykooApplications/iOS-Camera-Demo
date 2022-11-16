93cc5c492ee9035caa375a561280bcbe71dc434a
Roshan Mykoo <roshanjmykoo@gmail.com>
2022-11-15 22:52:04 -0500

    Following tutorial to get camera started
    No luck with getting it started, will try to combine material found.

--- CameraDemo/CameraDemo/ViewController.swift
+++ CameraDemo/CameraDemo/ViewController.swift
@@ -6,28 +6,38 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-    @IBOutlet weak var pictureSegueButton: UIButton!
-    @IBOutlet weak var videoSegueButton: UIButton!
-    @IBOutlet weak var qrSequeButton: UIButton!
     
-    // mainToPicture
-    // mainToVideo
-    // mainToQR
+    @IBOutlet weak var pictureSegueButton: UIButton! // mainToPicture
+    @IBOutlet weak var videoSegueButton: UIButton! // mainToVideo
+    @IBOutlet weak var qrSequeButton: UIButton! // mainToQR
+    
+    let captureSession = AVCaptureSession()
+    var previewLayer: CALayer!
+    var captureDevice: AVCaptureDevice!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setUpUI()
+    }
+    
+
+    func setUpUI() {
         pictureSegueButton.layer.cornerRadius = 5
         videoSegueButton.layer.cornerRadius = 5
         qrSequeButton.layer.cornerRadius = 5
+        
+        view.backgroundColor = UIColor.black
     }
     
+ 
     @IBAction func pictureButtonCLick(_ sender: Any) {
         performSegue(withIdentifier: "mainToPicture", sender: self)
     }
-    
     
     @IBAction func videoButtonClick(_ sender: Any) {
         performSegue(withIdentifier: "mainToVideo", sender: self)
