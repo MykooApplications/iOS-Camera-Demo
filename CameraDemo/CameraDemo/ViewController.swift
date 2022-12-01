87e45f5417b35c985a4321a970359e899b78d63e
Roshan Mykoo <roshanjmykoo@gmail.com>
2022-12-01 14:15:50 -0500

    super big changes
    refactor code for more streamlined application. better design

--- CameraDemo/CameraDemo/ViewController.swift
+++ CameraDemo/CameraDemo/ViewController.swift
@@ -5,19 +5,22 @@
 //  Created by Roshan Mykoo on 8/26/22.
 //
 
+//<a target="_blank" href="https://icons8.com/icon/110730/flash-on">Flash On</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+
+//<a target="_blank" href="https://icons8.com/icon/110822/flash-off">Flash Off</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+
 import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
-    
+    @IBOutlet weak var cameraSwitch: UISwitch!
+    @IBOutlet weak var flashButton: UIButton!
+    @IBOutlet weak var galleryButton: UIButton!
     @IBOutlet weak var pictureSegueButton: UIButton! // mainToPicture
-    @IBOutlet weak var videoSegueButton: UIButton! // mainToVideo
-    @IBOutlet weak var qrSequeButton: UIButton! // mainToQR
     
     let captureSession = AVCaptureSession()
     var previewLayer: CALayer!
     var captureDevice: AVCaptureDevice!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,27 +28,40 @@ class ViewController: UIViewController {
         setUpUI()
     }
     
-
+    
     func setUpUI() {
-        pictureSegueButton.layer.cornerRadius = 5
-        videoSegueButton.layer.cornerRadius = 5
-        qrSequeButton.layer.cornerRadius = 5
         
         view.backgroundColor = UIColor.black
+        
+       
+        pictureSegueButton.layer.cornerRadius = 0.5 * pictureSegueButton.bounds.size.width
+        pictureSegueButton.clipsToBounds = true
+        pictureSegueButton.setImage(UIImage(named:"thumbsUp.png"), for: .normal)
+       // pictureSegueButton.addTarget(self, action: #selector(thumbsUpButtonPressed), for: .touchUpInside)
+        
     }
     
- 
-    @IBAction func pictureButtonCLick(_ sender: Any) {
-        performSegue(withIdentifier: "mainToPicture", sender: self)
+    
+    @IBAction func captureButtonTap(_ sender: UIButton) {
+        
     }
     
-    @IBAction func videoButtonClick(_ sender: Any) {
-        performSegue(withIdentifier: "mainToVideo", sender: self)
+    
+    @IBAction func galleryButtonClick(_ sender: UIButton) {
+        
     }
     
-    @IBAction func qrButtonClick(_ sender: Any) {
-        performSegue(withIdentifier: "mainToQR", sender: self)
+    @IBAction func cameraSwitchToggle(_ sender: UISwitch) {
+        if sender.isOn {
+            print("[ViewController] video on")
+        } else {
+            print("[ViewController] camera on")
+        }
+        
     }
     
+    @IBAction func flashButtonTap(_ sender: UIButton) {
+        
+    }
 }
 
